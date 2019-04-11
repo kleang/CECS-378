b7db9012036c9bcc0ce0f084b02fb3d0e583242a
kleang <44731051+kleang@users.noreply.github.com>
2019-04-11 15:23:45 -0700

    Update MyEncrypt.py

--- MyEncrypt.py
+++ MyEncrypt.py
@@ -1,33 +1,39 @@
+
 import os
-from cyptography.hazmat.primitives.ciphers.aead import AESCCM
+import sys
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import padding
+from cryptography.hazmat.primitives import hashes, hmac
+from cryptography.exceptions import InvalidSignature
 
 IV_LENGTH = 16
 KEY_LENGTH = 32
 KEY_BITS = 256
 PAD_LENGTH = 128
 
-def Myencrypt(message, key):
-        if (len(key) != KEY_BITS):
-                raise ValueError("Key length must be 256 bits")
+def MyEncrypt(message, key):
+        if (len(key) < KEY_LENGTH):
+                raise ValueError("Key length is less than 32 bytes")
         padder =  padding.PKCS7(PAD_LENGTH).padder()
         messagePadded = padder.update(message)
         messagePadded += padder.finalize()
         IV = os.urandom(IV_LENGTH)
         encrypt = Cipher(algorithms.AES(key), modes.CBC(IV), backend = default_backend()).encryptor()
-        C = encryptor.update(message) + encryptor.finalize()
+        C = encrypt.update(messagePadded) + encrypt.finalize()
         return C, IV
 
-def MyfileEncrypt(filepath):
-        splitPath = path.splitext(filepath)
-        ext = splitPath[1]
+def MyFileEncrypt(filepath):
+        key = os.urandom(KEY_LENGTH)
+        name, ext = os.path.splitext(filepath)
         file = open(filepath, "rb")
         fileBytes = file.read()
         file.close()
-	C, IV = MyEncrypt(fileBytes, key)
-	file = open(filepath, "wb")
-	file.write(C)
-	file.close()
-	return C, IV, key, ext
+        C, IV = MyEncrypt(fileBytes, key)
+        file = open(filepath, "wb")
+        file.write(C)
+        file.close()
+        return C, IV, key, ext
 
 def MyDecrypt(ciphertext, key, IV):
         decrypt = Cipher(algorithms.AES(key), modes.CBC(IV), backend = default_backend()).decryptor()
@@ -37,9 +43,12 @@ def MyDecrypt(ciphertext, key, IV):
         message = message + unpadder.finalize()
         return message
 
-def MyfileDescrypt(filepath, key, IV, ext):
+def MyFileDecrypt(filepath, key, IV, ext):
         file = open(filepath, "rb")
         content = file.read()
         file.close()
         message = MyDecrypt(content, key, IV)
-        return message
+        return message, key, IV
+
+filepath = "text.txt"
+C, IV, key, ext = MyFileEncrypt(filepath)
